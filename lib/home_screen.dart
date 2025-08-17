0534f004f5506311de636179c3a6db6f4a97adfa
Ankit Patel <96364984+AnkitPatel6155@users.noreply.github.com>
2025-08-17 23:06:20 +0530

    some changes like add variables

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  var diceNumberleft = 1;
+  var diceNumberright = 2;
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dicee'),
+        title: const Text('Dicee', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white)),
         backgroundColor: Colors.red,
         centerTitle: true,
       ),
-          body: const Row(
-        children: [
-          Expanded(
-            child: Image(image: AssetImage('assets/images/dice1.png')),
+          backgroundColor: Colors.red[400],
+          body: GestureDetector(
+            onTap: () {
+              print('Tab');
+            },
+            child: Center(
+              child: Row(
+                      children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Image(image: AssetImage('images/dice${diceNumberleft}.png')),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Image(image: AssetImage('images/dice${diceNumberright}.png')),
+                ),
+              ),
+                      ],
+                  ),
+            ),
           ),
-          Expanded(
-            child: Image(image: AssetImage('assets/images/dice2.png')),
-          ),
-        ],
-    ),
     );
   }
 }
